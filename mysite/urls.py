1f4afd46438bc3a862b730dc339a97c8268acb55
KOngTNP <tanapollll23@gmail.com>
2022-01-14 13:57:55 +0700

    add myPost, editPost, updatePost and deletePost url

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('allPost/',views.allPost, name='allPost'),
     path('allPost/<str:post_id>',views.viewPost, name='viewPost'),
     path('createPost/',views.createPost, name='createPost'),
+
+    path('myPost/',views.myPost, name='myPost'),
+    path('myPost/edit/<str:post_id>',views.editPost, name='editPost'),
+    path('myPost/update/<str:post_id>',views.updatePost, name='updatePost'),
+    path('myPost/delete/<str:post_id>',views.deletePost, name='deletePost'),
 ]
