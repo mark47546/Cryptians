cceef63056de4c0572ec2f44f31364ebf5676d30
KOngTNP <tanapollll23@gmail.com>
2022-01-14 11:12:06 +0700

    add post and comment model to admin page

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django.db import models
+from tinymce.widgets import TinyMCE
 
-# Register your models here.
+class textEditorAdmin(admin.ModelAdmin):
+
+   list_display = ["content"]
+
+   formfield_overrides = {
+
+   models.TextField: {'widget': TinyMCE()}
+
+   }
+   
+admin.site.register(Post)
+admin.site.register(Comment)
+
