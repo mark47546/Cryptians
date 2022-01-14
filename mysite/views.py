6b795d8e2e1cb5f2ea2f8efbe059cabeaaa97d48
KOngTNP <tanapollll23@gmail.com>
2022-01-14 13:58:16 +0700

    add myPost, editPost, updatePost and deletePost functions

--- mysite/views.py
+++ mysite/views.py
@@ -34,3 +34,46 @@ def createPost(request):
 def viewPost(request,post_id):
     get_post_id = Post.objects.get(id=post_id)
     return render(request, 'post/view_post.html',{'post':get_post_id})
+
+
+def myPost(request):
+    #Query Data from model
+    search_post = request.GET.get('search')
+    if search_post:
+        search_post = Post.objects.filter(Q(title__icontains=search_post) & Q(title__icontains=search_post))
+        data = search_post.filter(posted_by=request.user)
+    else:
+        data = Post.objects.filter(posted_by=request.user)
+
+    count = Post.objects.filter(posted_by=request.user)
+    count_my_post = len(count)
+    return render(request,'post/my_post.html',{'post':data, 'count_my_post':count_my_post})
+
+
+def editPost(request,post_id):
+    posted_by = request.user
+    get_post_id = Post.objects.get(id=post_id)
+    if posted_by == get_post_id.posted_by:
+        form = CreatePostForm(instance=get_post_id)
+    else: 
+        pass
+    return render(request,'post/edit_post.html',{'form':form,'post':get_post_id, 'posted_by':posted_by})
+
+def updatePost(request,post_id):
+    get_post_id = Post.objects.get(id=post_id)
+    posted_by = request.user
+    form = UpdatePostForm(request.POST,request.FILES, instance=get_post_id)
+    if form.is_valid():
+        form.save()
+        return redirect("/myPost")
+    else:
+        return render(request,'post/edit_post.html',{'form':form,'post':get_post_id, 'posted_by':posted_by})
+
+def deletePost(request,post_id):
+    posted_by = request.user
+    get_post_id = Post.objects.get(id=post_id)
+    if posted_by == get_post_id.posted_by:
+        get_post_id.delete()
+    else:
+        pass
+    return redirect("/myPost")
