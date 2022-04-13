8c41a7c6abf98e235afee4f9849d07e0ffabac1d
KOngTNP <tanapollll23@gmail.com>
2022-04-13 18:24:27 +0700

    add django.contrib.humanize

--- cryptians/settings.py
+++ cryptians/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'social_django',
     'crispy_forms',
     'login',
