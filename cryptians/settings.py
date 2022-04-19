b9d2872ba342036d893707563cc4da2d840d1f8c
KOngTNP <tanapollll23@gmail.com>
2022-04-19 18:13:39 +0700

    install rest_framework

--- cryptians/settings.py
+++ cryptians/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'rest_framework',
+    'rest_framework.authtoken',
     'social_django',
     'crispy_forms',
     'login',
